3cb6440754d76fddb088e4b90101758bcdabfa2b
Teo <btebaldi@deskpaper.com>
2020-07-12 17:53:12 -0300

    analise de AMC a partir de 1995

--- Analise_AMC.R
+++ Analise_AMC.R
@@ -2,7 +2,7 @@
 rm(list = ls())
 
 # Carrega dados dos municipios
-load(file = "./dados.full.Rdata")
+load(file = "./Database/dados.1995plus.Rdata")
 
 # libraries
 library(readxl)
@@ -12,37 +12,87 @@ library(tidyr)
 library(urca)
 
 # mostra dados dos municipios
-head(dados.full)
+head(dados.1995plus)
 
 # carrega dados das AMCs
 # amc <- read_excel("Database/Amcs_70_91_00_v4.xlsx", range = cell_limits(c(1,1), c(NA,3)))
-amc <- read_excel("Database/Amcs_70_91_00_v5.xlsx",
-                  sheet = 2,
-                  range = cell_limits(c(2,1), c(NA,3)))
-
+amc <- read_excel("Database/Amcs_91_v1.xlsx",
+                  sheet = 1,
+                  range = "A1:E5661")
 
 # Retirar codigos ignorados, 99 e 0
-dados.full <- dados.full %>% filter(!(Municipio %in% c(99, 0)))
+dados.1995plus <- dados.1995plus %>% filter(!(Municipio %in% c(99, 0)))
+
+# dados.1995plus %>% filter(Municipio %in% c(99, 0))
 
 # Tabela com relacao de dados ignorados
-table <- tibble(cod=0, desc="NA", .rows = 27)
-table$cod <- c(119999, 129999, 139999, 149999, 159999, 169999, 
+tbl.ignorados <- tibble(cod=0, desc="NA", .rows = 27)
+tbl.ignorados$cod <- c(119999, 129999, 139999, 149999, 159999, 169999, 
                179999, 219999, 229999, 239999, 249999, 259999,
                269999, 279999, 289999, 299999, 319999, 329999, 
                339999, 359999, 419999, 429999, 439999, 509999,
                519999, 529999, 539999)
 
-table$desc <- c("Ro-Ignorado","Ac-Ignorado","Am-Ignorado","Rr-Ignorado","Pa-Ignorado","Ap-Ignorado",
+tbl.ignorados$desc <- c("Ro-Ignorado","Ac-Ignorado","Am-Ignorado","Rr-Ignorado","Pa-Ignorado","Ap-Ignorado",
                 "To-Ignorado","Ma-Ignorado","Pi-Ignorado","Ce-Ignorado","Rn-Ignorado","Pb-Ignorado",
                 "Pe-Ignorado","Al-Ignorado","Se-Ignorado","Ba-Ignorado","Mg-Ignorado","Es-Ignorado",
                 "Rj-Ignorado","Sp-Ignorado","Pr-Ignorado","Sc-Ignorado","Rs-Ignorado","Ms-Ignorado",
                 "Mt-Ignorado","Go-Ignorado","Df-Ignorado")
 
 # Retira da amostra os dados ignorados
-dados.full <- dados.full %>% filter(!(Municipio %in% table$cod))
+dados.1995plus <- dados.1995plus %>% filter(!(Municipio %in% tbl.ignorados$cod))
+# dados.1995plus %>% filter(Municipio %in% tbl.ignorados$cod)
+
+
+# ADICIONO MUNICIPIOS QUE NAO DECLARARAM NADA
+# Essa adicao é decorrente de investigacao de regiao que nao declarou nada.
+# O codigo para deteccao desta regiao pode ser encontrado mais abaixo.
+dados.1995plus <- dados.1995plus %>% add_row(Municipio=293245,
+                           Nao_admitido_ano=0,
+                           Nao_desligado_ano=0,
+                           ano=1998)
+
+dados.1995plus <- dados.1995plus %>% add_row(Municipio=292303,
+                                             Nao_admitido_ano=0,
+                                             Nao_desligado_ano=0,
+                                             ano=c(1997, 1998))
 
 # Junta os dados com os codigos das amcs
-dados.full <- dados.full %>% left_join(amc, by = c("Municipio" = "munic"))
+dados.1995plus <- dados.1995plus %>% left_join(amc, by = c("Municipio" = "munic"))
+
+# Verifica se há regioes sem AMC associada
+unique(dados.1995plus[is.na(dados.1995plus$amc), "Municipio"])
+
+# Mostra o total de amc em cada ano
+dados.1995plus %>%
+  group_by(ano) %>%
+  summarize(dist_amc = n_distinct(amc))
+
+
+# Procedimento prar investigar municipios que nao declararam nada
+for (j in 1995:2018) {
+  anoj <- dados.1995plus %>% filter(ano == j) %>% select(amc)
+  
+  for (i in 1995:2018) {
+    ano <- dados.1995plus %>% filter(ano == i) %>% select(amc)  
+    
+    cat(sprintf("\nNao esta em %d: %d", i, dplyr::setdiff(anoj$amc, ano$amc)))
+    cat(sprintf("\nNao esta em %d: %d", j, dplyr::setdiff(ano$amc, anoj$amc)))
+  }
+}
+rm(list = c("anoj", "ano", "i", "j"))
+
+# Detectado duas AMCS que nao tem informacao declarada
+# AMCs detectada como ausente
+# 293245 Umburanas      AMC: 293245 (População estimada [2019]	19.222 pessoas)
+# 292303 Novo Horizonte AMC: 292303 (População estimada [2019]	12.385 pessoas)
+
+# Essas informações foram consideradas como declaração zerada nos anos faltantes.
+
+
+# --------- Continuar analise ------------
+
+
 
 # Agrupa os dados em uma unica tabela
 dados.full.group <- dados.full %>%
@@ -136,3 +186,6 @@ cat(sprintf("\ndrift is not null: %d\n", sum(dados.deltas$phi1.p)))
 cat(sprintf("\ntau is not null (drift test): %d\n", sum(dados.deltas$tau2.p)))
 cat(sprintf("\ntau and drift is not null: %d\n", sum(dados.deltas$tau2.p & dados.deltas$phi1.p) ))
 cat(sprintf("\ntau is not null: %d\n", sum(dados.deltas$tau1.p)))
+
+
+dados.deltas[dados.deltas$phi1.p, ]
