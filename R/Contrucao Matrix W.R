e5ad234aed51c26768e122ac652d80dee21294a0
Teo <btebaldi@deskpaper.com>
2020-09-22 16:19:32 -0300

    matrix W

--- R/Contrucao Matrix W.R
+++ R/Contrucao Matrix W.R
@@ -9,9 +9,9 @@ library(ggplot2)
 library(stringr)
 library(scales)
 
-# Turn On or off the grafical print
-PrintGrafs <- TRUE
 
+# Suppress summarise info
+options(dplyr.summarise.inform = FALSE)
 
 #  ToDo: Fazer uma check de que a lista de municiopios eh sempre a mesma, acho
 #  que posso fazer uma lista "Basica" a qual será utilizada como gabarito para
@@ -21,24 +21,22 @@ PrintGrafs <- TRUE
 load(file = "./Database/codigos_municipios.RData")
 colnames(full) = "Muni"
 
-
-
 # carrego as informacoes de mesoregioes.
 meso.info <- readxl::read_excel("Database/Cadastro Municipios.xlsx", sheet = "Mesoregioes")
 
 # carrego as informacoes de PIB
 PIB.info <- readxl::read_excel("Database/PIB_Municipios_2010a2016.xlsx", 
-                       col_types = c("numeric", "numeric", "text", 
-                                     "numeric", "text", "text", "numeric", 
-                                     "text", "skip", "numeric", "text", 
-                                     "skip", "skip", "skip", "skip", "skip", 
-                                     "skip", "skip", "skip", "skip", "skip", 
-                                     "skip", "skip", "skip", "skip", "skip", 
-                                     "skip", "skip", "skip", "skip", "skip", 
-                                     "skip", "skip", "skip", "skip", "skip", 
-                                     "skip", "skip", "skip", "numeric",
-                                     "numeric", "numeric", "skip", "skip",
-                                     "skip"))
+                               col_types = c("numeric", "numeric", "text", 
+                                             "numeric", "text", "text", "numeric", 
+                                             "text", "skip", "numeric", "text", 
+                                             "skip", "skip", "skip", "skip", "skip", 
+                                             "skip", "skip", "skip", "skip", "skip", 
+                                             "skip", "skip", "skip", "skip", "skip", 
+                                             "skip", "skip", "skip", "skip", "skip", 
+                                             "skip", "skip", "skip", "skip", "skip", 
+                                             "skip", "skip", "skip", "numeric",
+                                             "numeric", "numeric", "skip", "skip",
+                                             "skip"))
 colnames(PIB.info) <- c("Ano",
                         "Cod_GrandeRegiao",
                         "Nome_GrandeRegiao",
@@ -53,7 +51,6 @@ colnames(PIB.info) <- c("Ano",
                         "Populacao",
                         "PIB_perCapita")
 
-head(PIB.info)
 
 # Seleciona o PIN e POPULACAO para o ano de 2016
 PIB.info %>% 
@@ -63,6 +60,11 @@ PIB.info %>%
   dplyr::summarise(PIB = sum(PIB_corrente), Pop = sum(Populacao)) %>% 
   apply(MARGIN = 2, FUN=sum) %>% format(big.mark=" ")
 
+PIB.info.2016 <- PIB.info %>% 
+  dplyr::filter(Ano == 2016) %>% 
+  dplyr::group_by(Cod_Meso) %>% 
+  dplyr::select(Cod_Meso, PIB_corrente, Populacao) %>% 
+  dplyr::summarise(PIB = sum(PIB_corrente), Pop = sum(Populacao))
 
 
 # Carrega a tabela de conexoes entre as regioes
@@ -122,15 +124,43 @@ for (col in 1:ncol(W.mat)) {
     if(Origem == Destino){
       W.mat[row, col] <- 0;
     } else {
-      W.mat[row, col] <- Connexoes.df %>% 
+      
+      Qtd_connex <- Connexoes.df %>% 
         filter(Meso_Origem == Origem, Meso_Destino == Destino) %>% 
         summarise(Total=n()) %>% pull(Total)
+      
+      # Coloca na matrix de connexao o total de conexao entre as cidades.
+      # W.mat[row, col] <- Qtd_connex
+      
+      
+      # Coloca na matrix de connexao o total do pib baseano na conexao entre as cidades.
+      if(Qtd_connex > 0){
+        W.mat[row, col] <- PIB.info.2016 %>% 
+          filter(Cod_Meso == Destino) %>% pull(Pop)  
+      } else {
+        W.mat[row, col] <- 0
+      }
     }
   }
 }
 
 
-readr::write_delim(x = W.mat)
+# --- Normaliza as colunas da matrix ----
+for (col in 1:ncol(W.mat)) {
+  somaColuna <- sum(W.mat[,col]);
+  W.mat[,col] <- W.mat[,col] / somaColuna
+}
+
+# --- Salva a matrix em arquivo .mat ----
+fileConn <- file("./Excel Export/Teste.mat")
+writeLines(c("137 137 // A 137 by 137 matrix"), fileConn)
+close(fileConn)
+
+write.table(x = W.mat, file = "./Excel Export/Teste.mat",
+            append = TRUE,
+            col.names = FALSE,
+            row.names = FALSE)
+
 
 
 
